64e590ebda8edc31e476d0b2d40eecfaeb5a3556
Shivangi <shivangikroxx@gmail.com>
2019-01-10 14:41:38 +0530

    Custom Api to check if the cafes are open or closed

--- loopback-getting-started/common/models/coffee-shop.js
+++ loopback-getting-started/common/models/coffee-shop.js
@@ -1,5 +1,32 @@
 'use strict';
 
 module.exports = function(Coffeeshop) {
+    Coffeeshop.status=function(callback){
+        let currentDate=new Date();
+        var currentHour= currentDate.getHours();
+        var OPEN_HOUR=6;
+        var CLOSE_HOUR=20;
+        console.log('Current hour is %d', currentHour);
+        var response;
+        if (currentHour>=OPEN_HOUR && currentHour<CLOSE_HOUR){
+          response='We are open for biz';
+        }
+        else {
+          response='Sorry, we are closed. We open daily from 6am to 8pm....';
 
+        }
+        callback(null,response);
+    };
+    Coffeeshop.remoteMethod(
+      'status', {
+        http:{
+          path:'/status',
+          verb:'get'
+        },
+        returns:{
+          arg:'status',
+          type:'string'
+        }
+      }
+    );
 };
---
+++ loopback-getting-started/server/boot/create-sample-models.js
@@ -0,0 +1,21 @@
+module.exports = function(app) {
+  app.dataSources.coffee_shops.automigrate('CoffeeShop', function(err) {
+    if (err) throw err;
+
+    app.models.CoffeeShop.create([{
+      name: 'Bel Cafe',
+      city: 'Vancouver'
+    }, {
+      name: 'Three Bees Coffee House',
+      city: 'San Mateo'
+    }, {
+      name: 'Caffe Artigiano',
+      city: 'Vancouver'
+    }], function(err, coffeeShops) {
+      if (err) throw err;
+
+      console.log('Models created: \n', coffeeShops);
+    });
+  });
+};
+
